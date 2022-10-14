659c573c87fe5db9ca336902d6bb52c7815f8112
couaoghislain <couaoghislain@hotmail.com>
2022-10-14 21:27:36 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Print Alphabet
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char n = "abcdefghijklmnopqrstuvwxyz";
+  putchar(n);
+	return (0);
+}
