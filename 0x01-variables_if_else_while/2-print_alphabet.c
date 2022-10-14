1d58ee2504b5d97a38f76f93df1ae011c3e20f62
couaoghislain <couaoghislain@hotmail.com>
2022-10-14 23:23:27 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,14 +9,11 @@
 
 int main(void)
 {
-/**
-*char ch = 'a';
-*for ( ; ch <= 'z'; ch++ 
-*{putchar(ch);} ascii number from A=65; Z=90; a=97; z=122
-*/
 	int i;
-
+	
 	for (i = 97; i <= 122; i++)
-		putchar( i );
+	{
+		putchar(i) ;
+	}
 	return (0);
 }
