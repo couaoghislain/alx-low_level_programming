b35288da833d979694055b770441c2ccf03e7b62
couaoghislain <couaoghislain@hotmail.com>
2022-10-14 15:08:21 -0700

    ppp

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,16 +9,14 @@
 
 int main(void)
 {
-/**	char ch = 'a';
-
-	for ( ; ch <= 'z'; ch++ )
-	{
-		putchar(ch);
-	} ascii number from A=65; Z=90; a=97; z=122
-	*/
+/**
+*char ch = 'a';
+*for ( ; ch <= 'z'; ch++ 
+*{putchar(ch);} ascii number from A=65; Z=90; a=97; z=122
+*/
 	int i;
 
-	for (i=97; i<=122; i++)
-		putchar(i);
+	for (i = 97; i <= 122; i++)
+		putchar( i );
 	return (0);
 }
