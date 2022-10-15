afb7eb635011eae0036b367dfce043aaf9b3e3bf
couaoghislain <couaoghislain@hotmail.com>
2022-10-15 20:14:47 +0100

    Print Alphabet in lower case without q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
-s
+#include <stdio.h>
+
+/**
+*main - Print Alphabet in lower case
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+    if (i != 'e' && i != 'q')
+      putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
