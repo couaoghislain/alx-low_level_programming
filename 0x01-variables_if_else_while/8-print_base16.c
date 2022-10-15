28694fe1c00ced3e343fde7b3204cfd7d9ad670d
couaoghislain <couaoghislain@hotmail.com>
2022-10-15 18:56:06 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,14 @@
-s
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Print number of base 16
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+  putchar('%x');
+	return (0);
+}
