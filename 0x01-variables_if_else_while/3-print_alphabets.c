b7ff32929a868fd475d3032b78d8a4819947ce55
couaoghislain <couaoghislain@hotmail.com>
2022-10-15 20:02:27 +0100

    Print Alphabet in lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,22 @@
-s
+#include <stdio.h>
+
+/**
+*main - Print Alphabet in lower and upper case
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char i;
+  
+  for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+  for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
