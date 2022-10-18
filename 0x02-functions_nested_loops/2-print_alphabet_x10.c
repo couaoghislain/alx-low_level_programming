249a04f16f95864d488fa5fd9ee5764915406751
couaoghislain <couaoghislain@hotmail.com>
2022-10-18 08:37:53 -0700

    Task 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+	}
+	_putchar('\n');
+}
