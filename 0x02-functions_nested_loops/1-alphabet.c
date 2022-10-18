a6edfcdbe47d2ecd5b570f594c3adb7637430bbc
couaoghislain <couaoghislain@hotmail.com>
2022-10-18 15:33:05 +0100

    task 01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print all alphabet in lowercase
+ * is written by couao
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+  for(i = 'a'; i <= 'z'; i++)
+  {
+  _putchar(i);
+  }
+	_putchar('\n');
+	return (0);
+}
