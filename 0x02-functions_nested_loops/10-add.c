d898b14ded8873ec16e5f2ca2f60ea4c1209114e
couaoghislain <couaoghislain@hotmail.com>
2022-10-18 13:07:08 -0700

    ee

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integer
+ * @n : first integer
+ * @j : secon integer
+ * Return: result of add
+ */
+
+int add(int n, int j)
+{
+	int r = n + j;
+
+	_putchar(r);
+	return (0);
+}
