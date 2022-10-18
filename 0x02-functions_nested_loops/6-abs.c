8c72aab24a90163ad84c99150da4e4b7f5648869
couaoghislain <couaoghislain@hotmail.com>
2022-10-18 12:15:28 -0700

    eee

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print the abs of the number
+ * @n:the number
+ * Return: abs of the number
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
