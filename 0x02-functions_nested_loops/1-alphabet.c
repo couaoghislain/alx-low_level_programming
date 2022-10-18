b4676018fd5542bed0d1e6da4fa096472fbd756a
couaoghislain <couaoghislain@hotmail.com>
2022-10-18 15:45:43 +0100

    task 01 ok

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,12 @@
 
 /**
 * print_alphabet - print all alphabet in lowercase
-* is written by couao
 */
 
 void print_alphabet(void)
 {
 	char i;
+	
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
