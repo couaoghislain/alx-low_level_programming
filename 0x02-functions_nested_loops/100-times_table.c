036e50c34f2a4a25a1fc61ee607c4e1724af7dc8
couaoghislain <couaoghislain@hotmail.com>
2022-10-19 13:55:16 -0700

    100timetable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,12 +16,12 @@ void print_times_table(int n)
 	}
 	else
 	{
-		for (rone = 0; rone <= 9; rone++)
+		for (rone = 0; rone <= n; rone++)
 		{
 			_putchar('0');
 			_putchar(',');
 			_putchar(' ');
-			for (cone = 1; cone <= 9; cone++)
+			for (cone = 1; cone <= n; cone++)
 			{
 				d = (rone * cone);
 				if ((d / 10) > 0)
@@ -33,7 +33,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 				_putchar((d % 10) + '0');
-				if (cone < 9)
+				if (cone < n)
 				{
 					_putchar(',');
 					_putchar(' ');
