6002c40a7c9efa42f247e8d9ea5f8e17d2d04389
couaoghislain <couaoghislain@hotmail.com>
2022-10-19 04:26:15 -0700

    98 Battery

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Function that print all natural numbers from n to 98
+ * @n: number to be used
+ * Return: Always 0 (success)
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			if (n != 98)
+				_putchar(', ');
+			n++;
+		}
+	}
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n);
+			if (n != 98)
+				_putchar(', ');
+			n--;
+		}
+	}
+	else
+		_putchar(98);
+	_putchar('\n');
+}
