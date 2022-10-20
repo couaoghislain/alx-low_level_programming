0b3fd0f375aed229389e7d933459a549f8cdbc04
couaoghislain <couaoghislain@hotmail.com>
2022-10-20 10:42:34 -0700

    number

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
  * _putchar - write the char
  * @c:the character to be print
- * Return: 1 if success else -1
+ * Return: 1 if success, 0 if false
+ * on error, -1 return
  */
 
 int _putchar(char c)
