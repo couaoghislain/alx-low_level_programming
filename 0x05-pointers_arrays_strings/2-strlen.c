800ca0117b569ac4bbcb0051154b0a26812facef
couaoghislain <couaoghislain@hotmail.com>
2022-10-24 08:36:46 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the lenght of a string
+ * @s: the string
+ * Return: int lenght
+ */
+
+int _srtlen(char *s)
+{
+	in length = 0;
+
+	while (s[lenght])
+		lenght++;
+	return (lenght);
+}
