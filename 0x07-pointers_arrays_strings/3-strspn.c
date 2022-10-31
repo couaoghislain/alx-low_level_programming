64ca7a5c514036ca2caa406a0c5197be26802608
couaoghislain <couaoghislain@hotmail.com>
2022-10-31 10:10:40 -0700

    Task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *  _strspn - Gets the length of a prefix substring
+ *  @s: The string to be searched
+ *  @accept: The prefix to be measured
+ *  Return: The number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int j;
+
+	while (*s)
+	{
+		for (i = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				j++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (i);
+		}
+		s++
+	}
+	return (i);
+}
