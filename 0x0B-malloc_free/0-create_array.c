d1ee419a21f2a17bff00b10147c87fc89b155ea6
couaoghislain <couaoghislain@hotmail.com>
2022-11-09 11:52:41 -0800

    ee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,11 +17,14 @@ char *create_array(unsigned int size, char c)
 	
 	if (size == 0)
 		return (NULL);
+
 	array = malloc(sizeof(char) * size);
 	
 	if (array == NULL)
 		return (NULL);
+
 	for (index = 0; index < size; index++)
 		array[index] = c;
+
 	return (array);
 }
