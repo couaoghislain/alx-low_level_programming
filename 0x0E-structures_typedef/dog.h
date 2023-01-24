cf31d70a9724e9eec4fca66ef5c47a1d9fe72a80
couaoghislain <couaoghislain@hotmail.com>
2023-01-24 19:34:00 +0100

    Completed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,14 +3,14 @@
 
 /*
  * File: dog.h
- * Desc: Header file that defines a new type struct dog
+ * Desc: Header file that defines a new type struct dog.
  */
 
 /**
- * struct dog - A new type describing a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
